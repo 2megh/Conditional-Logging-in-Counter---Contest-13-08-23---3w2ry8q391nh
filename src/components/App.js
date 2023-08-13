e77e8cb22366930a1350955a1f4c9d8d896270b6
megha <meghadongre6@gmail.com>
2023-08-13 11:10:51 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,37 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
-class App extends React.Component{
-  
-  handleClick(){
-    this.setState({count: this.state.count + 1})
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
   }
-  constructor(props){
-    super(props)
-    this.state = {count:0}
-    this.handleClick = this.handleClick.bind(this)
-  }
-  shouldComponentUpdate(nextProps,nextState){
-    const isCountEven = nextState.count %2 ===0;
 
-    if(isCountEven){
+  shouldComponentUpdate(nextProps, nextState) {
+    const isCountEven = nextState.count % 2 === 0;
+
+    if (isCountEven) {
       return true;
     }
+
     return false;
   }
 
-  handleClick =()=>{
-    this.setState({count: this.state.count +1 });
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
-  render(){
-    console.log(`Rendering with count:-${this.state.count}`)
-     return(
-      render(){
-        console.log(`Rendering with count:-${this.state.count}`)
-       return(
+  render() {
+    console.log(`Rendering with count:-${this.state.count}`);
+    return (
       <div>
         <span id="count">{this.state.count}</span>
-        <button id="incr-btn" onClick={this.handleClick}>Increment</button>
-        <button id ="incr-btn" onClick={this.handleClick}>Increment</button>
+        <button id="incr-btn" onClick={this.handleClick}>
+          Increment
+        </button>
       </div>
-    )
-       }
     );
   }
-
 }
 
 export default App;
